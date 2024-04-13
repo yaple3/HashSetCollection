156ab506d329f01891ba46574c7c9532d050f5bf
yaple3 <yaple3@JB204-L26-W-D>
2024-04-12 23:19:11 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace HashSetCollection
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            HashSet<string> agenda = ["Mow Lawn", "Weed Garden", "Vacuum Rugs", "Clean Bathroom", "Make Bed"];
+
+            HashSet<string> priorAgenda = ["Sweep Floors", "Wash Car", "Walk Dog", "Visit Grandma", "Run Errands"];
+
+            HashSet<string> toDoList = ["Exercise", "Study", "Sleep", "Walk Dog", "Make Bed"];
+
+            Console.WriteLine("Combined list of older and newer agenda");
+            agenda.UnionWith(priorAgenda);
+            foreach (string task in agenda)
+            {
+                Console.WriteLine(task);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("tasks that are in both lists");
+            HashSet<string> bothLists = [];
+            bothLists = agenda;
+            bothLists.IntersectWith(toDoList);
+            foreach (string task in bothLists)
+            {
+                Console.WriteLine(task);
+            }
+        }
+    }
+}
